38ee5f77a6e0907d84ec0ace54c03c6fdde8b5eb
Havens <me@hvs.cx>
2019-12-21 13:47:45 +0100

    Add Tailwind package

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,6 +51,19 @@
     </div>
 </div>
 
+
+<div class="md:flex container border p-4">
+    <div class="md:flex-shrink-0">
+        <img class="rounded-lg md:w-56" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80" alt="Woman paying for a purchase">
+    </div>
+    <div class="mt-4 md:mt-0 md:ml-6">
+        <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Marketing</div>
+        <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding customers for your new business</a>
+        <p class="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+    </div>
+</div>
+
+
 @if ($image->path != null)
 <script>
     window.onbeforeunload = function() {
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
+const tailwindcss = require('tailwindcss');
+
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')],
+    });
