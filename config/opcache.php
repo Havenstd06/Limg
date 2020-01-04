46fd7e4ef0eec528b397bf6b0fde1ae54712f9c7
Havens <me@hvs.cx>
2020-01-04 18:58:08 +0100

    Laravel Opcache

---
+++ config/opcache.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'url' => env('OPCACHE_URL', config('app.url')),
+    'prefix' => 'opcache-api',
+    'verify_ssl' => true,
+    'verify_host' => 2, // 0 for disabled
+    'headers' => [],
+    'directories' => [
+        base_path('app'),
+        base_path('bootstrap'),
+        base_path('public'),
+        base_path('resources'),
+        base_path('routes'),
+        base_path('storage'),
+        base_path('vendor'),
+    ],
+    'exclude' => [
+        'test',
+        'Test',
+        'tests',
+        'Tests',
+        'stub',
+        'Stub',
+        'stubs',
+        'Stubs',
+        'dumper',
+        'Dumper'
+    ]
+];
