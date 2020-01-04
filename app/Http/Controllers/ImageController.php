8a228d0d16ef0a88c4e0718edb42d31b0fe057c8
Thomas Drumont <me@hvs.cx>
2020-01-04 18:10:46 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -30,15 +30,15 @@ class ImageController extends Controller
         $user = (auth()->user()) ? auth()->user() : User::findOrFail(1);
 
         $newName = Str::random(7);
-        $newFullName = $newName . '.' . $request->file('image')->getClientOriginalExtension();
+        $newFullName = $newName.'.'.$request->file('image')->getClientOriginalExtension();
         $request->file('image')->move(('storage/images'), $newFullName);
 
         $image = new Image;
         $image->name = $newName;
         $image->extension = pathinfo($newFullName, PATHINFO_EXTENSION);
-        $image->path = '/i/' . $newFullName;
+        $image->path = '/i/'.$newFullName;
         $image->user_id = $user->id;
-        $image->is_public = (!$user->always_public) ? 0 || (!Auth::check() || $user->always_public) : 1;
+        $image->is_public = (! $user->always_public) ? 0 || (! Auth::check() || $user->always_public) : 1;
         $image->save();
 
         notify()->success('You have successfully upload image!');
@@ -49,7 +49,7 @@ class ImageController extends Controller
     public function get($image)
     {
         if (strpos($image, '.') !== false) { // Afficher la page avec l'extension
-            $imageLink = Image::where('path', '/i/' . $image)->firstOrFail();
+            $imageLink = Image::where('path', '/i/'.$image)->firstOrFail();
 
             return response()->download($imageLink->fullpath, null, [], null);
         } else { // Afficher le view image
@@ -105,12 +105,12 @@ class ImageController extends Controller
 
     public function download(Image $image)
     {
-        return ($image->title) ? response()->download($image->fullpath, Str::slug($image->title, '-') . '.' . $image->extension) : response()->download($image->fullpath);
+        return ($image->title) ? response()->download($image->fullpath, Str::slug($image->title, '-').'.'.$image->extension) : response()->download($image->fullpath);
     }
 
     public function build($image, $size)
     {
-        $imageLink = Image::where('path', '/i/' . $image)->firstOrFail();
+        $imageLink = Image::where('path', '/i/'.$image)->firstOrFail();
 
         $w = InterImage::make($imageLink->fullpath)->width();
         $h = InterImage::make($imageLink->fullpath)->height();
