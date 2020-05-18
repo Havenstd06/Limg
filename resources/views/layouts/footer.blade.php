4116f2d96a98cc3bf7bdd9000fbcf1dc302d57f2
Havens <me@hvs.cx>
2020-05-18 15:37:46 +0200

    new footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,18 @@
-<footer class="w-full p-4 text-center text-gray-800 shadow-md dark:bg-midnight dark:text-white font-firacode">
-  <i class="far fa-heart"></i> <a href="https://github.com/havenstd06/limg" target="_nofollow">LIMG</a> {{ date('Y') }}
+<footer class="text-gray-200 bg-gray-800 dark:bg-midnight dark:text-white font-firacode">
+  <div class="flex flex-wrap items-center px-16 py-3">
+    <div class="w-full text-center lg:w-1/5 lg:text-left">
+      <a class="text-xl font-semibold text-white" href="{{ route('home') }}">{{ config('app.name', 'Laravel') }}</a>
+    </div>
+    <div class="w-full mt-4 text-center lg:w-3/5 lg:mt-0">
+      <span class="block mx-3 mb-4 font-semibold md:inline-block md:mb-0">
+        Made with <i class="far fa-heart"></i> by <a href="https://github.com/havenstd06" target="_nofollow">Havens</a> — 2019-{{ date('Y') }}
+      </span>
+    </div>
+    <div class="flex justify-center w-full my-2 item-center lg:justify-end lg:w-1/5 lg:my-0">
+      <a href="{{ config('app.discordUrl') }}" target="_nofollow">
+        <span class="relative mr-2 font-semibold" style="bottom:7px">Open-Source on GitHub</span> 
+        <i class="fa-2x fab fa-github"></i>
+      </a>
+    </div>
+  </div>
 </footer>
