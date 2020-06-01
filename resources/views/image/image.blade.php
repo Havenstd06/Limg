feccf62224aef77f384609550387fd14a69db7bc
Havens <me@hvs.cx>
2020-06-01 13:48:40 +0200

    Disable turbolinks when download

--- resources/views/image/image.blade.php
+++ resources/views/image/image.blade.php
@@ -81,7 +81,7 @@
               <a href="#" onclick="javascript:changeText();location.reload();" target="_nofollow" id=lnk class="p-2 pr-4 font-semibold text-center text-gray-800 bg-gray-200">Go</a>
             </div>
           </div>
-          <a href="{{ route('image.download', ['image' => $image->pageName]) }}">
+          <a href="{{ route('image.download', ['image' => $image->pageName]) }}" data-turbolinks="false">
             <button type="button" class="w-full px-4 py-2 font-normal text-gray-200 transition duration-300 ease-in-out border border-purple-500 rounded btn-outline-primary focus:outline-none focus:shadow-outline hover:bg-purple-700 hover:text-white">
               <i class="fa fa-download"></i> {{ __('Download') }}
             </button>
