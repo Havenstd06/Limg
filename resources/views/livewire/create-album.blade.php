5d805e1765e5956c4a612d94994a73dda585d37a
Havens <me@hvs.cx>
2020-06-04 16:37:07 +0200

    Disable turbolinks when create album

--- resources/views/livewire/create-album.blade.php
+++ resources/views/livewire/create-album.blade.php
@@ -51,7 +51,7 @@
                     <div class="flex justify-end">  
                     <span class="inline-flex ml-3 rounded-md shadow-sm">
                         <button wire:click.prevent="create" type="submit" class="inline-flex justify-center px-4 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700">
-                            <span wire:loading.remove wire:target="create">
+                            <span wire:loading.remove wire:target="create" data-turbolinks="false">
                                 Create
                             </span>
                             <span wire:loading wire:target="create">
