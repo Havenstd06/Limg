80b651a8708b8c40bb8fd479c448b5dc9c70a3b9
Havens <me@hvs.cx>
2020-06-04 19:18:24 +0200

    Lel

--- app/User.php
+++ app/User.php
@@ -2,10 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use Notifiable;
 
