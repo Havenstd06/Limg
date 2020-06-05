10140b82a3a134ae3bed0b0acf21059ea150bdaa
Havens <me@hvs.cx>
2020-06-06 01:36:45 +0200

    Backup system

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,6 +63,11 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
         ],
+
+        'backups' => [
+            'driver' => 'local',
+            'root'   => storage_path('backups'), // that's where your backups are stored by default: storage/backups
+        ],
     ],
 
 ];
