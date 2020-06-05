be8df9562094dcc6bda8c2091a5c8ed89a1dcfeb
Havens <me@hvs.cx>
2020-06-05 23:05:55 +0200

    Add backpack :smile:

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,3 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-question'></i> Users</a></li>
---
+++ routes/backpack/custom.php
@@ -0,0 +1,18 @@
+<?php
+
+// --------------------------
+// Custom Backpack Routes
+// --------------------------
+// This route file is loaded automatically by Backpack\Base.
+// Routes you generate using Backpack\Generators will be placed here.
+
+Route::group([
+    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'middleware' => [
+        config('backpack.base.web_middleware', 'web'),
+        config('backpack.base.middleware_key', 'admin'),
+    ],
+    'namespace'  => 'App\Http\Controllers\Admin',
+], function () { // custom admin routes
+    Route::crud('user', 'UserCrudController');
+}); // this should be the absolute last line of this file
