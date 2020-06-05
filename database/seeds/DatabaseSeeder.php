5cf829c8a2c82ed4780de09662de4cb09c8d1d62
Havens <me@hvs.cx>
2020-06-05 21:47:15 +0200

    Add users seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(ImagesTableSeeder::class);
         $this->call(DomainsTableSeeder::class);
         $this->call(AlbumsSeeder::class);
