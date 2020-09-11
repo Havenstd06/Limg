895fc79fd63057892d39147eab7958ec9cc09ae4
Havens <me@hvs.cx>
2020-09-11 23:00:54 +0200

    lowercase migration

--- database/migrations/2020_09_11_224313_remove_short_link_from_users_table.php
+++ database/migrations/2020_09_11_224313_remove_short_link_from_users_table.php
@@ -13,7 +13,7 @@ class RemoveShortLinkFromUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('short_link');
         });
     }
@@ -25,7 +25,7 @@ class RemoveShortLinkFromUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
         });
     }
