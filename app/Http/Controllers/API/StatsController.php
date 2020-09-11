8ae0b9e0b3ad872ca07f012a9857e142eb1226e1
Havens <me@hvs.cx>
2020-09-11 20:27:24 +0200

    more global stats

--- app/Http/Controllers/API/StatsController.php
+++ app/Http/Controllers/API/StatsController.php
@@ -6,6 +6,7 @@ use App\Album;
 use App\Http\Controllers\Controller;
 use App\Image;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class StatsController extends Controller
@@ -19,92 +20,48 @@ class StatsController extends Controller
     {
         $data = [];
 
-        $stats = [
+        $all_time = [
             'images' => Image::count(),
             'albums' => Album::count(),
             'users'  => User::count(),
         ];
 
-        $data['global'] = $stats;
-
-        return $data;
-    }
-
-    /**
-     * Return site stats.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function user(Request $request, $username)
-    {
-        $data = [];
-
-        $user = User::where('username', $username)->with('images')->first();
-        $private_key = key($request->query());
-
-        if (! $user) {
-            return response()->json([
-                'success' => false,
-                'error'   => 'User not found!',
-            ], 404);
-        }
-
-        $public_stats = [
-            'images_count' => $user->images()->where('is_public', 1)->count(),
-            'albums_count' => $user->albums()->where('is_public', 1)->count(),
+        $last_day = [
+            'images' => Image::where('created_at', '>=', \Carbon\Carbon::now()->subDay())->count(),
+            'albums' => Album::where('created_at', '>=', \Carbon\Carbon::now()->subDay())->count(),
+            'users'  => User::where('created_at', '>=', \Carbon\Carbon::now()->subDay())->count(),
         ];
 
-        $public_info = [
-            'username'      => $user->username,
-            'description'   => $user->description,
-            'role'          => $user->role,
-            'avatar'        => config('app.url').'/'.$user->avatar,
-            'public_images' => $user->images()->where('is_public', 1)->orderBy('created_at', 'DESC')->get(),
+        $last_week = [
+            'images' => Image::where('created_at', '>=', \Carbon\Carbon::now()->subWeek())->count(),
+            'albums' => Album::where('created_at', '>=', \Carbon\Carbon::now()->subWeek())->count(),
+            'users'  => User::where('created_at', '>=', \Carbon\Carbon::now()->subWeek())->count(),
         ];
-        $data['public_stats'] = $public_stats;
-        $data['public_info'] = $public_info;
 
-        if ($private_key == null) {
-            return $data;
-        }
+        $last_month = [
+            'images' => Image::where('created_at', '>=', \Carbon\Carbon::now()->subMonth())->count(),
+            'albums' => Album::where('created_at', '>=', \Carbon\Carbon::now()->subMonth())->count(),
+            'users'  => User::where('created_at', '>=', \Carbon\Carbon::now()->subMonth())->count(),
+        ];
 
-        $keys = User::all()->makeVisible('api_token')->pluck('api_token')->toArray();
-        if (in_array($private_key, $keys)) {
-            if ($user->api_token != $private_key) {
-                return response()->json([
-                    'success' => false,
-                    'error'   => 'The given key is not the same as the requested user!',
-                ], 403);
-            }
+        $last_half_year = [
+            'images' => Image::where('created_at', '>=', \Carbon\Carbon::now()->subMonths(6))->count(),
+            'albums' => Album::where('created_at', '>=', \Carbon\Carbon::now()->subMonths(6))->count(),
+            'users'  => User::where('created_at', '>=', \Carbon\Carbon::now()->subMonths(6))->count(),
+        ];
 
-            $private_stats = [
-                'all_images_count'     => $user->images->count(),
-                'public_images_count'  => $user->images()->where('is_public', 1)->count(),
-                'private_images_count' => $user->images()->where('is_public', 0)->count(),
-                'all_albums_count'     => $user->albums->count(),
-                'public_albums_count'  => $user->albums()->where('is_public', 1)->count(),
-                'private_albums_count' => $user->albums()->where('is_public', 0)->count(),
-            ];
+        $last_year = [
+            'images' => Image::where('created_at', '>=', \Carbon\Carbon::now()->subYear())->count(),
+            'albums' => Album::where('created_at', '>=', \Carbon\Carbon::now()->subYear())->count(),
+            'users'  => User::where('created_at', '>=', \Carbon\Carbon::now()->subYear())->count(),
+        ];
 
-            $private_info = [
-                'email'               => $user->email,
-                'domain'              => $user->domain,
-                'discord_webhook_url' => $user->webhook_url,
-                'dark_theme'          => $user->style,
-                'always_public'       => $user->always_public,
-                'short_link'          => $user->short_link,
-                'private_images'      => $user->images()->where('is_public', 0)->orderBy('created_at', 'DESC')->get(),
-                'all_images'          => $user->images()->orderBy('created_at', 'DESC')->get(),
-            ];
-
-            $data['private_stats'] = $private_stats;
-            $data['private_info'] = $private_info;
-        } else {
-            return response()->json([
-                'success' => false,
-                'error'   => 'Invalid key!',
-            ], 401);
-        }
+        $data['all_time'] = $all_time;
+        $data['last_day'] = $last_day;
+        $data['last_week'] = $last_week;
+        $data['last_month'] = $last_month;
+        $data['last_half_year'] = $last_half_year;
+        $data['last_year'] = $last_year;
 
         return $data;
     }
